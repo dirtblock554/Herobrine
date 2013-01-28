9e15ff856b9be92f3d8b99ddc46aca6f61844777
Krecklow, Nicholas R <owner@nkrecklow.com>
2013-01-27 19:12:01 -0500

    Torch action as a demo.

--- src/com/kreckin/herobrined/actions/PlaceTorch.java
+++ src/com/kreckin/herobrined/actions/PlaceTorch.java
@@ -2,12 +2,25 @@ package com.kreckin.herobrined.actions;
 
 import com.kreckin.herobrined.api.IActionResult;
 import com.kreckin.herobrined.impl.Action;
+import com.kreckin.herobrined.impl.ActionResult;
+import com.kreckin.herobrined.util.BlockUtil;
+import com.kreckin.herobrined.util.PlayerUtil;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class PlaceTorch extends Action {
 
     @Override
     public IActionResult callAction(Player player, Object[] metadata) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Block block = player.getWorld().getBlockAt(PlayerUtil.getNearbyLocation(player, 10));
+        if (BlockUtil.isValid(block)) {
+            block.setType(Material.REDSTONE_TORCH_ON);
+            Location loc = block.getLocation();
+            return (new ActionResult("Done.", "Location: " + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ()));
+        } else {
+            return (new ActionResult("Failed, could not find a proper location!"));
+        }
     }
 }
